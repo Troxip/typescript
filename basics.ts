2943a577177ce4b21cd396c7114a16e220b074bf
Abdurahim Gaforov <abdurahim.gaforov9547@gmail.com>
2024-03-23 15:54:15 +0100

    Done

---
+++ basics.ts
@@ -0,0 +1,41 @@
+// Primitives
+
+let age: number;
+age = 12;
+
+let userName: string;
+
+userName = "Max";
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+let hobbies: string[];
+
+hobbies = ["Sport", "Cooking", "Football"];
+
+type Person = {
+  name: string;
+  age: number;
+};
+
+let person: Person;
+
+person = {
+  name: "Max",
+  age: 32,
+};
+
+// person = {
+//   isEmployee: true,
+// };
+
+let people: Person[];
+
+// Type inference
+
+let course: string | number = "React - The complete guide";
+
+course = 12341;
+console.log(course);
